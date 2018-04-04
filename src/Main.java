21b6514a81ea9e3cf0b1780a869fe4961b82c7ad
Parker Smith <parker@parkersmith.us>
2018-04-04 00:48:06 -0600

    Implement Dijstra's arithmetic evaluation algorithm with stacks

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    StackInterface<String> ops = new StackList<>();
+	    StackInterface<Double> vals = new StackList<>();
+
+	    while (!StdIn.isEmpty())
+        {
+            String s = StdIn.readString();
+
+            switch(s)
+            {
+                case "(":
+                    break;
+                case "+":
+                case "*":
+                    ops.push(s);
+                    break;
+                case ")":
+                    String op = ops.pop();
+                    if (op.equals("+"))
+                    {
+                        vals.push(vals.pop() + vals.pop());
+                    }
+                    else if (op.equals("*"))
+                    {
+                        vals.push(vals.pop() * vals.pop());
+                    }
+                    break;
+                default:
+                    vals.push(Double.parseDouble(s));
+            }
+        }
+
+        System.out.println(vals.pop());
     }
 }
